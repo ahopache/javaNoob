10270e778e2b44ac9b5e6a8fdf22afe5ca399bd2
Assis <assishenrique@gmail.com>
2020-10-15 14:20:17 -0300

    Create UtilFTPServer

---
+++ src/main/java/util/UtilFTPServer.java
@@ -0,0 +1,27 @@
+/**
+ * encode: utf-8
+ *
+ * @author Assis Henrique Oliveira Pacheco
+ * @version: 0.9
+ *
+ * # PortuguesBR
+ * Classe que deve definir e gerenciar um servidor de FTP
+ *
+ * # English
+ * Class to manage an FTP server
+ */
+package util;
+
+public class UtilFTPServer {
+    public void start(){
+        //TODO:
+    }
+
+    public void addUser(String userTest, String passTest) {
+        //TODO: 
+    }
+
+    public void stop() {
+        //TODO:
+    }
+}
